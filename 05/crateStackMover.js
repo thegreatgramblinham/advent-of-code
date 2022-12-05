ca2d010f3b25b4a9f8496c1c5198d61069fcf504
Sam Hudock <samhudock@gmail.com>
2022-12-05 08:08:07 -0500

    Adds puzzle input for day 5

---
+++ 05/crateStackMover.js
@@ -0,0 +1,39 @@
+const moveInstructionsString = "";
+
+
+/// MAIN START ///
+
+main();
+
+/// MAIN END ///
+
+function main()
+{
+    var initialStackStates =
+    [
+        //1
+        [],
+        //2
+        [],
+        //3
+        [],
+        //4
+        [],
+        //5
+        [],
+        //6
+        [],
+        //7
+        [],
+        //8
+        [],
+        //9
+        []
+    ]
+
+    for (let i = 0; i < moveInstructionsString.length; i++)
+    {
+        const currentChar = moveInstructionsString[i];
+       
+    }
+}
