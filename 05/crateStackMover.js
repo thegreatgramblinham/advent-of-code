f051d5a93f735e489018d9edd4d9e3633788540a
Sam Hudock <shudock@altair.com>
2022-12-05 10:28:06 -0500

    Adds solutions for day 5 puzzles

--- 05/crateStackMover.js
+++ 05/crateStackMover.js
@@ -1,5 +1,29 @@
-const moveInstructionsString = "";
+var moveInstructionsString = "move,6,from,4,to,3;move,5,from,8,to,9;move,1,from,4,to,5;move,1,from,4,to,5;move,2,from,2,to,7;move,2,from,1,to,6;move,9,from,6,to,1;move,12,from,3,to,5;move,1,from,8,to,4;move,3,from,1,to,5;move,1,from,6,to,7;move,10,from,5,to,2;move,14,from,5,to,1;move,8,from,7,to,9;move,11,from,2,to,9;move,1,from,3,to,9;move,11,from,1,to,5;move,2,from,1,to,9;move,1,from,4,to,8;move,6,from,1,to,5;move,1,from,8,to,3;move,16,from,5,to,1;move,4,from,1,to,3;move,1,from,5,to,6;move,4,from,3,to,4;move,1,from,6,to,7;move,21,from,9,to,6;move,2,from,1,to,9;move,2,from,4,to,9;move,5,from,9,to,4;move,9,from,1,to,6;move,6,from,4,to,6;move,1,from,6,to,2;move,1,from,7,to,6;move,1,from,3,to,2;move,8,from,6,to,9;move,3,from,1,to,8;move,1,from,2,to,1;move,13,from,6,to,3;move,1,from,1,to,9;move,2,from,1,to,6;move,3,from,8,to,4;move,4,from,4,to,9;move,3,from,1,to,3;move,22,from,9,to,8;move,1,from,2,to,9;move,6,from,8,to,9;move,15,from,6,to,5;move,5,from,8,to,9;move,11,from,9,to,8;move,13,from,5,to,1;move,1,from,6,to,5;move,1,from,9,to,3;move,21,from,8,to,3;move,3,from,5,to,3;move,11,from,1,to,2;move,25,from,3,to,1;move,5,from,1,to,7;move,20,from,1,to,7;move,1,from,6,to,7;move,16,from,3,to,9;move,8,from,9,to,6;move,1,from,1,to,5;move,5,from,9,to,4;move,2,from,2,to,1;move,2,from,9,to,4;move,1,from,9,to,4;move,1,from,8,to,4;move,1,from,5,to,2;move,3,from,4,to,6;move,1,from,4,to,7;move,9,from,7,to,6;move,5,from,4,to,6;move,7,from,7,to,2;move,1,from,1,to,6;move,11,from,2,to,5;move,10,from,5,to,1;move,1,from,6,to,8;move,1,from,5,to,7;move,24,from,6,to,1;move,12,from,1,to,4;move,12,from,4,to,8;move,2,from,2,to,7;move,3,from,7,to,2;move,5,from,2,to,8;move,9,from,8,to,9;move,9,from,8,to,5;move,1,from,9,to,1;move,14,from,1,to,8;move,11,from,7,to,9;move,4,from,1,to,3;move,7,from,1,to,2;move,3,from,3,to,7;move,12,from,9,to,7;move,8,from,7,to,2;move,4,from,9,to,2;move,1,from,3,to,6;move,5,from,5,to,9;move,14,from,2,to,1;move,8,from,9,to,4;move,6,from,4,to,5;move,5,from,5,to,7;move,1,from,8,to,2;move,2,from,4,to,6;move,4,from,7,to,3;move,10,from,8,to,4;move,2,from,3,to,6;move,7,from,7,to,6;move,10,from,4,to,8;move,5,from,1,to,6;move,8,from,2,to,1;move,7,from,6,to,8;move,9,from,6,to,5;move,16,from,1,to,6;move,2,from,3,to,9;move,1,from,7,to,4;move,2,from,9,to,1;move,14,from,6,to,7;move,1,from,6,to,3;move,2,from,6,to,3;move,9,from,5,to,7;move,3,from,1,to,6;move,3,from,3,to,7;move,5,from,5,to,9;move,3,from,6,to,2;move,1,from,6,to,2;move,12,from,8,to,2;move,5,from,2,to,1;move,2,from,1,to,3;move,25,from,7,to,1;move,1,from,4,to,6;move,2,from,3,to,9;move,26,from,1,to,9;move,2,from,1,to,8;move,1,from,6,to,8;move,1,from,7,to,1;move,7,from,8,to,1;move,7,from,1,to,5;move,1,from,1,to,2;move,2,from,8,to,6;move,32,from,9,to,8;move,1,from,6,to,5;move,5,from,2,to,9;move,1,from,9,to,7;move,24,from,8,to,3;move,1,from,6,to,9;move,3,from,2,to,5;move,1,from,7,to,9;move,4,from,9,to,3;move,8,from,8,to,7;move,18,from,3,to,7;move,20,from,7,to,8;move,6,from,8,to,9;move,6,from,5,to,1;move,8,from,9,to,4;move,3,from,5,to,4;move,8,from,8,to,4;move,2,from,5,to,2;move,3,from,1,to,5;move,4,from,3,to,7;move,6,from,2,to,9;move,3,from,3,to,6;move,6,from,4,to,5;move,2,from,6,to,3;move,1,from,3,to,1;move,4,from,3,to,8;move,8,from,4,to,3;move,4,from,3,to,7;move,4,from,4,to,5;move,4,from,9,to,5;move,3,from,3,to,4;move,3,from,4,to,9;move,1,from,1,to,4;move,2,from,1,to,5;move,7,from,7,to,8;move,4,from,7,to,4;move,1,from,6,to,7;move,1,from,1,to,5;move,1,from,3,to,8;move,11,from,5,to,9;move,17,from,9,to,8;move,13,from,8,to,4;move,1,from,4,to,8;move,4,from,7,to,1;move,4,from,8,to,3;move,6,from,5,to,4;move,3,from,3,to,6;move,2,from,1,to,9;move,1,from,9,to,5;move,1,from,3,to,5;move,5,from,5,to,9;move,2,from,1,to,8;move,21,from,8,to,6;move,2,from,8,to,4;move,4,from,9,to,6;move,1,from,9,to,7;move,19,from,4,to,1;move,28,from,6,to,5;move,7,from,4,to,2;move,28,from,5,to,3;move,1,from,9,to,4;move,1,from,4,to,2;move,1,from,7,to,8;move,1,from,8,to,9;move,13,from,1,to,3;move,8,from,2,to,8;move,3,from,1,to,2;move,5,from,8,to,5;move,1,from,2,to,7;move,1,from,9,to,7;move,1,from,2,to,3;move,2,from,7,to,9;move,1,from,2,to,6;move,1,from,9,to,1;move,9,from,3,to,9;move,3,from,9,to,1;move,1,from,6,to,8;move,21,from,3,to,7;move,7,from,9,to,4;move,2,from,4,to,2;move,1,from,8,to,6;move,7,from,1,to,4;move,7,from,7,to,8;move,4,from,5,to,9;move,10,from,7,to,1;move,7,from,3,to,9;move,1,from,7,to,9;move,1,from,5,to,3;move,3,from,3,to,5;move,10,from,4,to,2;move,1,from,3,to,7;move,2,from,4,to,9;move,3,from,9,to,1;move,3,from,7,to,1;move,1,from,6,to,4;move,1,from,1,to,2;move,1,from,3,to,4;move,2,from,4,to,3;move,1,from,7,to,4;move,4,from,8,to,9;move,1,from,4,to,9;move,3,from,1,to,9;move,12,from,1,to,7;move,2,from,9,to,5;move,12,from,9,to,7;move,5,from,5,to,1;move,1,from,8,to,5;move,4,from,1,to,4;move,1,from,9,to,6;move,1,from,3,to,4;move,3,from,8,to,3;move,1,from,1,to,7;move,8,from,2,to,5;move,2,from,8,to,1;move,10,from,7,to,1;move,4,from,9,to,5;move,2,from,5,to,8;move,11,from,5,to,4;move,6,from,7,to,2;move,2,from,2,to,1;move,1,from,7,to,5;move,1,from,5,to,1;move,2,from,4,to,8;move,1,from,6,to,9;move,8,from,4,to,3;move,8,from,1,to,7;move,7,from,1,to,2;move,4,from,3,to,9;move,1,from,9,to,6;move,7,from,2,to,1;move,5,from,2,to,3;move,2,from,7,to,8;move,5,from,8,to,4;move,2,from,9,to,3;move,1,from,8,to,1;move,6,from,3,to,5;move,10,from,3,to,1;move,3,from,5,to,3;move,3,from,2,to,1;move,1,from,5,to,4;move,6,from,4,to,5;move,1,from,6,to,2;move,3,from,4,to,7;move,1,from,9,to,4;move,2,from,3,to,1;move,1,from,9,to,8;move,1,from,3,to,7;move,4,from,4,to,8;move,2,from,7,to,4;move,8,from,5,to,9;move,2,from,8,to,6;move,2,from,4,to,3;move,2,from,3,to,4;move,4,from,9,to,7;move,1,from,8,to,7;move,2,from,6,to,9;move,2,from,8,to,9;move,1,from,2,to,9;move,1,from,7,to,8;move,1,from,2,to,7;move,19,from,7,to,6;move,1,from,8,to,1;move,2,from,4,to,8;move,5,from,6,to,1;move,2,from,7,to,2;move,2,from,2,to,8;move,2,from,1,to,8;move,4,from,8,to,2;move,3,from,2,to,8;move,6,from,9,to,5;move,8,from,6,to,3;move,26,from,1,to,6;move,1,from,5,to,3;move,1,from,1,to,5;move,8,from,3,to,1;move,1,from,3,to,7;move,3,from,9,to,2;move,4,from,2,to,6;move,26,from,6,to,1;move,1,from,7,to,5;move,3,from,8,to,4;move,2,from,8,to,2;move,7,from,1,to,2;move,1,from,5,to,9;move,2,from,4,to,6;move,9,from,6,to,2;move,18,from,1,to,7;move,6,from,7,to,1;move,6,from,5,to,6;move,1,from,1,to,2;move,19,from,2,to,7;move,1,from,4,to,2;move,9,from,7,to,1;move,3,from,6,to,7;move,1,from,9,to,4;move,1,from,2,to,3;move,8,from,7,to,8;move,4,from,6,to,5;move,2,from,6,to,3;move,1,from,4,to,2;move,4,from,5,to,1;move,8,from,8,to,7;move,17,from,7,to,8;move,3,from,3,to,1;move,1,from,2,to,8;move,8,from,8,to,4;move,8,from,8,to,7;move,1,from,8,to,2;move,7,from,7,to,6;move,1,from,2,to,7;move,5,from,7,to,8;move,7,from,1,to,6;move,10,from,6,to,1;move,4,from,7,to,9;move,3,from,9,to,7;move,1,from,7,to,2;move,6,from,4,to,2;move,7,from,1,to,5;move,4,from,2,to,5;move,16,from,1,to,9;move,3,from,2,to,7;move,2,from,4,to,9;move,4,from,1,to,6;move,5,from,7,to,4;move,4,from,6,to,3;move,1,from,7,to,4;move,1,from,6,to,9;move,1,from,8,to,5;move,4,from,3,to,2;move,2,from,5,to,3;move,3,from,6,to,2;move,3,from,2,to,1;move,9,from,5,to,8;move,1,from,3,to,1;move,10,from,8,to,1;move,1,from,8,to,5;move,16,from,9,to,2;move,1,from,3,to,2;move,12,from,1,to,9;move,1,from,9,to,2;move,3,from,1,to,6;move,2,from,1,to,9;move,3,from,6,to,8;move,20,from,2,to,7;move,16,from,9,to,7;move,1,from,7,to,5;move,2,from,5,to,9;move,2,from,2,to,3;move,2,from,8,to,5;move,3,from,9,to,7;move,2,from,5,to,2;move,1,from,4,to,6;move,2,from,1,to,4;move,23,from,7,to,5;move,4,from,8,to,5;move,7,from,7,to,1;move,16,from,5,to,7;move,1,from,6,to,5;move,1,from,2,to,4;move,2,from,3,to,9;move,1,from,2,to,3;move,13,from,5,to,1;move,1,from,3,to,8;move,1,from,9,to,4;move,19,from,1,to,9;move,2,from,1,to,9;move,22,from,9,to,8;move,14,from,8,to,5;move,12,from,5,to,3;move,21,from,7,to,9;move,14,from,9,to,7;move,1,from,8,to,6;move,9,from,3,to,7;move,1,from,3,to,2;move,4,from,4,to,1;move,1,from,2,to,4;move,1,from,3,to,9;move,6,from,8,to,9;move,4,from,1,to,7;move,2,from,5,to,9;move,6,from,4,to,5;move,4,from,7,to,4;move,1,from,5,to,3;move,5,from,9,to,7;move,2,from,3,to,1;move,6,from,9,to,6;move,1,from,1,to,6;move,2,from,4,to,2;move,8,from,7,to,5;move,20,from,7,to,5;move,2,from,5,to,6;move,4,from,9,to,5;move,1,from,1,to,3;move,1,from,3,to,4;move,1,from,2,to,7;move,1,from,4,to,9;move,9,from,6,to,3;move,2,from,4,to,3;move,28,from,5,to,3;move,1,from,8,to,3;move,1,from,8,to,1;move,1,from,2,to,8;move,1,from,6,to,2;move,1,from,8,to,1;move,6,from,5,to,7;move,1,from,5,to,1;move,1,from,9,to,2;move,1,from,1,to,3;move,1,from,9,to,7;move,2,from,1,to,2;move,11,from,3,to,8;move,3,from,8,to,6;move,3,from,6,to,9;move,25,from,3,to,7;move,4,from,3,to,8;move,4,from,2,to,3;move,9,from,8,to,9;move,2,from,3,to,7;move,3,from,8,to,2;move,11,from,9,to,7;move,1,from,9,to,1;move,4,from,7,to,3;move,1,from,1,to,5;move,23,from,7,to,2;move,12,from,2,to,3;move,2,from,3,to,9;move,12,from,2,to,1;move,2,from,3,to,9;move,1,from,5,to,4;move,1,from,2,to,5;move,1,from,9,to,4;move,1,from,5,to,9;move,2,from,4,to,2;move,3,from,1,to,4;move,1,from,2,to,1;move,10,from,3,to,2;move,7,from,7,to,3;move,11,from,7,to,9;move,5,from,3,to,1;move,1,from,4,to,5;move,11,from,2,to,3;move,9,from,9,to,3;move,3,from,9,to,4;move,2,from,4,to,8;move,1,from,5,to,6;move,13,from,1,to,5;move,3,from,3,to,8;move,3,from,7,to,2;move,1,from,7,to,4;move,3,from,8,to,3;move,8,from,3,to,8;move,4,from,4,to,5;move,2,from,8,to,2;move,8,from,8,to,3;move,1,from,6,to,3;move,2,from,2,to,8;move,6,from,5,to,2;move,3,from,2,to,8;move,1,from,1,to,7;move,2,from,9,to,3;move,3,from,5,to,4;move,2,from,8,to,6;";
 
+var partSeparator = ',';
+var instructionSeparator = ";";
+
+var stackStates =
+[
+    //1
+    ['J', 'H', 'G', 'M', 'Z', 'N', 'T', 'F'],
+    //2
+    ['V', 'W', 'J'],
+    //3
+    ['G', 'V', 'L', 'J', 'B', 'T', 'H'],
+    //4
+    ['B', 'P', 'J', 'N', 'C', 'D', 'V', 'L'],
+    //5
+    ['F', 'W', 'S', 'M', 'P', 'R', 'G'],
+    //6
+    ['G', 'H', 'C', 'F', 'B', 'N', 'V', 'M'],
+    //7
+    ['D', 'H', 'G', 'M', 'R'],
+    //8
+    ['H', 'N', 'M', 'V', 'Z', 'D'],
+    //9
+    ['G', 'N', 'F', 'H']
+]
 
 /// MAIN START ///
 
@@ -9,31 +33,96 @@ main();
 
 function main()
 {
-    var initialStackStates =
-    [
-        //1
-        [],
-        //2
-        [],
-        //3
-        [],
-        //4
-        [],
-        //5
-        [],
-        //6
-        [],
-        //7
-        [],
-        //8
-        [],
-        //9
-        []
-    ]
+    let moveQuantity = 0;
+    let fromStackIndex = 0;
+    let toStackIndex = 0;
+    let partSeparatorCountForCurrentInstruction = 0;
 
+    let totalInstructionsProcessed = 0;
+
+    let currentNumberString = "";
     for (let i = 0; i < moveInstructionsString.length; i++)
     {
         const currentChar = moveInstructionsString[i];
-       
+
+        if (currentChar == instructionSeparator)
+        {
+            // We've completed the last number parse.
+            toStackIndex = parseInt(currentNumberString)-1;
+            currentNumberString = "";
+
+            // Operate on the instruction.
+            //applyStackInstruction(moveQuantity, fromStackIndex, toStackIndex);
+            applyBulkStackInstruction(moveQuantity, fromStackIndex, toStackIndex);
+            totalInstructionsProcessed++;
+
+            // Reset state.
+            partSeparatorCountForCurrentInstruction = 0;
+            moveQuantity = 0;
+            fromStackIndex = 0;
+            toStackIndex = 0;
+        }
+        else if (currentChar == partSeparator)
+        {
+            partSeparatorCountForCurrentInstruction++;
+
+            switch (partSeparatorCountForCurrentInstruction)
+            {
+                case 2:
+                    moveQuantity = parseInt(currentNumberString);
+                    currentNumberString = "";
+                    break;
+                case 4:
+                    fromStackIndex = parseInt(currentNumberString)-1;
+                    currentNumberString = "";
+                    break;
+            }
+        }
+        else if (partSeparatorCountForCurrentInstruction%2 == 1)
+        {
+            // If were in a number parsing section, record that.
+            currentNumberString += currentChar;
+        }
+    }
+
+    // Compile the puzzle output.
+    let topStackMembers = "";
+    for (let i = 0; i < stackStates.length; i++)
+    {
+        const currentStack = stackStates[i]
+        topStackMembers += currentStack[currentStack.length-1];
+    }
+
+    console.log("Total instructions processed: "+totalInstructionsProcessed);
+    console.log("Top stack members: "+topStackMembers);
+}
+
+// First puzzle solution.
+function applyStackInstruction(moveQuantity, fromIndex, toIndex)
+{
+    //console.log("processing instruction: move"+moveQuantity+",from"+fromIndex+",to"+toIndex);
+    for (let i = 0; i < moveQuantity; i++)
+    {
+        var popValue = stackStates[fromIndex].pop();
+        stackStates[toIndex].push(popValue);
+    }
+}
+
+// Secord puzzle solution.
+function applyBulkStackInstruction(moveQuantity, fromIndex, toIndex)
+{
+    //console.log("processing instruction: move"+moveQuantity+",from"+fromIndex+",to"+toIndex);
+    var moveArray = [];
+    for (let i = 0; i < moveQuantity; i++)
+    {
+        let popValue = stackStates[fromIndex].pop();
+        moveArray.push(popValue);
+    }
+
+    //console.log("move array length: "+ moveArray.length)
+    for (let i = 0; i < moveQuantity; i++)
+    {
+        let movedPopValue = moveArray.pop();
+        stackStates[toIndex].push(movedPopValue);
     }
 }
